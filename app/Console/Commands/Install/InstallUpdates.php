aa7c4aa543db98c97bf75ebceb91f2ad3616e091
Christoph Fischer <chris@toph.de>
2023-05-22 15:39:13 +0200

    feat: Automatischer Updatebefehl install:updates

---
+++ app/Console/Commands/Install/InstallUpdates.php
@@ -0,0 +1,138 @@
+<?php
+/*
+ * Pfarrplaner
+ *
+ * @package Pfarrplaner
+ * @author Christoph Fischer <chris@toph.de>
+ * @copyright (c) Christoph Fischer, https://christoph-fischer.org
+ * @license https://www.gnu.org/licenses/gpl-3.0.txt GPL 3.0 or later
+ * @link https://codeberg.org/pfarrplaner/pfarrplaner
+ * @version git: $Id$
+ *
+ * Sponsored by: Evangelischer Kirchenbezirk Balingen, https://www.kirchenbezirk-balingen.de
+ *
+ * Pfarrplaner is based on the Laravel framework (https://laravel.com).
+ * This file may contain code created by Laravel's scaffolding functions.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace App\Console\Commands\Install;
+
+use App\Services\PasswordService;
+use App\Services\UpdateService;
+use App\User;
+use Illuminate\Console\Command;
+use Illuminate\Console\Command\Install;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Str;
+use Spatie\Permission\Models\Role;
+
+class InstallUpdates extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'install:updates {--dry-run : Only show which actions would be performed by an update}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Install updates';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $files = UpdateService::getUpdateableFiles();
+
+        if ($files->count() == 0) {
+            $this->line('<info>INFO</info> Done installing updates.');
+            return Command::SUCCESS;
+        }
+
+        $actions = UpdateService::getUpdateActions($files);
+
+        if ($this->options('dry-run')) {
+            $this->line('The following '.$files->count().' files will be affected by the next update: ');
+            foreach ($files as $file) {
+                $this->line(' - '.$file);
+            }
+            $this->line('');
+            $this->line('The following actions will be required by the next update:');
+            foreach ($actions as $action => $description) {
+                $this->line(' - '.$description);
+            }
+            return Command::SUCCESS;
+        }
+
+        // pull
+        $this->getOutput()->section('Fetching updates to '.$files->count().' files');
+        exec('git pull');
+
+
+        // composer
+        if ($actions['composer']) {
+            $this->getOutput()->section('Composer');
+            passthru('composer update');
+            $this->line('');
+        }
+
+        // npm install
+        if ($actions['npm']) {
+            $this->getOutput()->section('NPM packages');
+            passthru('npm install');
+            $this->line('');
+        }
+
+        // npm run prod
+        if ($actions['webpack']) {
+            $this->getOutput()->section('NPM build');
+            passthru('npm run prod');
+            $this->line('');
+        }
+
+        // art migrate
+        if ($actions['migrations']) {
+            $this->getOutput()->section('Database migrations');
+            Artisan::call('migrate');
+            $this->line('');
+        }
+
+        // art optimize
+        if ($actions['optimizations']) {
+            $this->getOutput()->section('Optimizations');
+            Artisan::call('optimize');
+            $this->line('');
+        }
+
+
+        $this->line('<info>INFO</info> Done installing updates.');
+        return Command::SUCCESS;
+    }
+
+    protected function hasFileChanges(Collection $files, $tag) {
+        return $files->filter(function ($item) use ($tag) {
+            return Str::startsWith($item, $tag);
+        })->count() > 0;
+    }
+}
---
+++ app/Services/UpdateService.php
@@ -0,0 +1,101 @@
+<?php
+/*
+ * Pfarrplaner
+ *
+ * @package Pfarrplaner
+ * @author Christoph Fischer <chris@toph.de>
+ * @copyright (c) Christoph Fischer, https://christoph-fischer.org
+ * @license https://www.gnu.org/licenses/gpl-3.0.txt GPL 3.0 or later
+ * @link https://codeberg.org/pfarrplaner/pfarrplaner
+ * @version git: $Id$
+ *
+ * Sponsored by: Evangelischer Kirchenbezirk Balingen, https://www.kirchenbezirk-balingen.de
+ *
+ * Pfarrplaner is based on the Laravel framework (https://laravel.com).
+ * This file may contain code created by Laravel's scaffolding functions.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace App\Services;
+
+use Illuminate\Support\Collection;
+use Illuminate\Support\Str;
+
+class UpdateService
+{
+
+    /**
+     * Get all files that would be affected by pulling an update
+     * @return Collection
+     */
+    public static function getUpdateableFiles(): Collection {
+        exec('git fetch');
+        $log = shell_exec('git log --name-status origin/main');
+        $currentCommit = shell_exec('git rev-parse HEAD');
+
+        $log = explode("\n", substr($log, 0, strpos($log, $currentCommit)));
+        $files = collect();
+
+        foreach ($log as $line) {
+            if (str_contains($line, "\t")) {
+                $files->push(Str::afterLast($line, "\t"));
+            }
+        }
+
+        return $files;
+    }
+
+    /**
+     * Check if an update is available
+     * @param Collection|null $files optional file collection to check against
+     * @return bool
+     */
+    public static function hasUpdate(Collection $files = null): bool
+    {
+        $files ??= static::getUpdateableFiles();
+        return $files->count() > 0;
+    }
+
+    /**
+     * Check which actions would be required by an update
+     * @param Collection|null $files optional file collection to check against
+     * @return array Actions
+     */
+    public static function getUpdateActions(Collection $files = null): array
+    {
+        $files ??= static::getUpdateableFiles();
+        $actions = [];
+
+        if (static::hasFileChanges('composer.', $files)) $actions['composer'] = 'Composer updates';
+        if (static::hasFileChanges('package.json', $files)) $actions['npm'] = 'NPM package installs';
+        if (static::hasFileChanges('resources/js/', $files)) $actions['webpack'] = 'Webpack (compiling resources)';
+        if (static::hasFileChanges('database/migrations/', $files)) $actions['migrations'] = 'Database migrations';
+        $actions['optimizations'] = 'Cache optimizations';
+        return $actions;
+    }
+
+    /**
+     * Check if an update has changes to a specific path
+     * @param String $path (beginning of) path
+     * @param Collection|null $files optional file collection to check against
+     * @return bool
+     */
+    public static function hasFileChanges($path, Collection $files = null): bool
+    {
+        return $files->filter(function ($item) use ($path) { return Str::startsWith($item, $path); })->count() > 0;
+    }
+
+
+}
