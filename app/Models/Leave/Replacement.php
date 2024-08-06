acea95a16c2017ad72552ec4e5f82e3b4d0ccf58
Christoph Fischer <chris@toph.de>
2024-08-06 14:36:21 +0200

    fix: Poolmaster in Ausgabe nicht gegendert

--- app/Models/Leave/Replacement.php
+++ app/Models/Leave/Replacement.php
@@ -111,7 +111,7 @@ class Replacement extends Model
                     $from = max(Carbon::parse($poolmaster->start . ' 0:00:00'), $this->from);
                     $to = min(Carbon::parse($poolmaster->end . ' 23:59:59'), $this->to);
 
-                    $texts[] = $poolmaster->user->name . ' [Poolmaster "' . $poolmaster->pool->name . '"]'
+                    $texts[] = $poolmaster->user->name . ' [Poolmaster:in "' . $poolmaster->pool->name . '"]'
                         . ' (' . StringTool::durationText($from, $to) . ')';
                 }
             }
