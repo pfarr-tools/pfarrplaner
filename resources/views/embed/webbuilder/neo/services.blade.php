e9be31c230fd96fdcb1642ca3dfdc5c6754f610d
Christoph Fischer <chris@toph.de>
2025-07-22 17:17:32 +0200

    chore: PHPDoc

--- resources/views/embed/webbuilder/neo/services.blade.php
+++ resources/views/embed/webbuilder/neo/services.blade.php
@@ -1,4 +1,5 @@
 @php
+    /** @var \Illuminate\Support\Collection $occurences */
     $events = $occurences->groupBy(function (App\Models\Calendar\Occurence $item, int $key) {
                 return $item->start->format('Ymd');
             });
