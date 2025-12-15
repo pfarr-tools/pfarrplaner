359521d35118407a0deae1fc550456891506ac73
Christoph Fischer <chris@toph.de>
2025-12-15 15:43:21 +0100

    fix: Vertikale Höhe fehlt im Backend

--- resources/js/Pages/Layouts/AdminLayout.vue
+++ resources/js/Pages/Layouts/AdminLayout.vue
@@ -70,7 +70,7 @@
                 </template>
             </Topbar>
 
-            <div class="px-2 pt-4 overflow-scroll">
+            <div class="px-2 pt-4 overflow-scroll vh-100">
                 <h1 v-if="title" class="m-0 mb-4 text-dark" :key="title">{{ title }}</h1>
                 <slot name="before-flash" />
                 <transition name="fade">
