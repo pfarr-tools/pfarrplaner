9cf9984be764b1a2b2d705ab91bb62fc26f2cb1a
Christoph Fischer <chris@toph.de>
2026-05-19 11:51:54 +0200

    fix: Leeres Arbeitsverzeichnis für PDF-Downloads abgefangen.

--- app/Documents/PDF.php
+++ app/Documents/PDF.php
@@ -30,17 +30,28 @@
 
 namespace App\Documents;
 
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\View;
+use Spatie\Browsershot\ChromiumResult;
 use Spatie\Browsershot\Browsershot;
+use Spatie\Browsershot\Exceptions\ElementNotFound;
+use Spatie\Browsershot\Exceptions\RemoteConnectionException;
+use Spatie\Browsershot\Exceptions\UnsuccessfulResponse;
+use Symfony\Component\Process\Exception\ProcessFailedException;
+use Symfony\Component\Process\Process;
 use Symfony\Component\HttpFoundation\BinaryFileResponse;
 
 class PDF extends Browsershot
 {
-
-
-
-    public static function fromView($viewName, $data) {
+    /**
+     * @param string $viewName
+     * @param array $data
+     * @return static
+     */
+    public static function fromView($viewName, $data)
+    {
         return static::html(View::make($viewName, $data)->render())
+            ->setCustomTempPath(storage_path('app/tmp'))
             ->format('A4')
             ->margins(10, 20, 10, 20)
             ->showBackground()
@@ -55,13 +66,62 @@ class PDF extends Browsershot
     public function download($filename): BinaryFileResponse
     {
         $tempFile = storage_path('app/tmp/'.$filename);
+        File::ensureDirectoryExists(dirname($tempFile));
         $this->save($tempFile);
 
         return response()->download($tempFile, $filename, ['Content-Type' => 'application/pdf'])
             ->deleteFileAfterSend(true);
-
     }
 
+    /**
+     * Start Browsershot with a guaranteed working directory even if the runtime
+     * environment does not provide a valid current directory.
+     *
+     * @param array $command
+     * @return string
+     */
+    protected function callBrowser(array $command): string
+    {
+        $fullCommand = $this->getFullCommand($command);
+        $workingDirectory = base_path() ?: getcwd() ?: storage_path('app');
 
+        $process = $this->isWindows()
+            ? new Process($fullCommand, $workingDirectory, $this->getWindowsEnv())
+            : Process::fromShellCommandline($fullCommand, $workingDirectory);
 
+        $process->setTimeout($this->timeout);
+
+        $this->chromiumResult = null;
+        $process->run();
+
+        $rawOutput = rtrim($process->getOutput());
+        $this->chromiumResult = new ChromiumResult(json_decode($rawOutput, true));
+
+        if ($process->isSuccessful()) {
+            $result = $this->chromiumResult?->getResult();
+
+            $this->cleanupTemporaryOptionsFile();
+
+            return $result;
+        }
+
+        $this->cleanupTemporaryOptionsFile();
+        $process->clearOutput();
+        $exitCode = $process->getExitCode();
+        $errorOutput = $process->getErrorOutput();
+
+        if ($exitCode === 4) {
+            throw RemoteConnectionException::make(rtrim($errorOutput));
+        }
+
+        if ($exitCode === 3) {
+            throw UnsuccessfulResponse::make($this->url, $errorOutput ?? '');
+        }
+
+        if ($exitCode === 2) {
+            throw ElementNotFound::make($this->additionalOptions['selector']);
+        }
+
+        throw new ProcessFailedException($process);
+    }
 }
