1a90af10133aa44d459f97c7606157ff1b9b62b1
Christoph Fischer <chris@toph.de>
2026-05-29 09:05:10 +0200

    fix: Reiter "Trauungen" lädt nicht

--- app/Models/Calendar/Day.php
+++ app/Models/Calendar/Day.php
@@ -80,7 +80,7 @@ class Day extends Model
      */
     public function getLiturgyAttribute(): array
     {
-        return LiturgyService::getLiturgyInfoByDate($this);
+        return LiturgyService::getLiturgyInfoByDate($this->date);
     }
 // END ACCESSORS
 
