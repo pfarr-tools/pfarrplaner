3bf2720b18724bb03d20d8a2d9e63ab9b9972a0b
Christoph Fischer <chris@toph.de>
2026-07-03 11:18:31 +0200

    fix: Girocodes können nicht gedruckt werden.

--- app/Reports/GiroCodeQRReport.php
+++ app/Reports/GiroCodeQRReport.php
@@ -107,15 +107,15 @@ class GiroCodeQRReport extends AbstractPDFDocumentReport
         $data = $request->validate(
             [
                 'city' => 'required|int|exists:cities,id',
-                'start' => 'required|date|date_format:d.m.Y',
-                'end' => 'required|date|date_format:d.m.Y',
+                'start' => 'required|date|date_format:Y-m-d',
+                'end' => 'required|date|date_format:Y-m-d',
                 'copies' => 'required|int',
             ]
         );
 
         $allServices = Service::where('city_id', $data['city'])
-            ->between(Carbon::createFromFormat('d.m.Y', $data['start']),
-                      Carbon::createFromFormat('d.m.Y', $data['end'])
+            ->between(Carbon::createFromFormat('Y-m-d', $data['start']),
+                      Carbon::createFromFormat('Y-m-d', $data['end'])
             )->ordered()->get();
 
 
